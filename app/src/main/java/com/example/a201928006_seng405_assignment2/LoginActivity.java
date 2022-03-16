8088cfd82e48e62ad67ae1aa4b30c7d8e8014637
emrebyrk <emrebayrakpersonal@gmail.com>
2022-03-16 22:09:13 +0300

    Login Screen

---
+++ app/src/main/java/com/example/a201928006_seng405_assignment2/LoginActivity.java
@@ -0,0 +1,14 @@
+package com.example.a201928006_seng405_assignment2;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+}
